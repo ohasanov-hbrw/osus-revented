e0775edae63b14310ea9a1bbfb7e107b6ed6cc5c
ohasanov-hbrw <ka9an1sn00b@gmail.com>
2022-10-16 20:28:49 +0300

    32bit builds.

--- windowsmakefast.sh
+++ windowsmakefast.sh
@@ -1,1 +1,1 @@
-make run CC="i686-w64-mingw32-gcc --static" name="osus.exe" ldflags="-lstdc++ -Lvendor/raylib/src -lraylib -Lvendor/zip/build -lzip -lpthread -lwinmm -lgdi32 -lopengl32" -j12
+make run CC="i686-w64-mingw32-gcc -m32 --static" name="osus.exe" ldflags="-lstdc++ -Lvendor/raylib/src -lraylib -Lvendor/zip/build -lzip -lpthread -lwinmm -lgdi32 -lopengl32" -j12
