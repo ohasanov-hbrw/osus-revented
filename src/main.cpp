fc5de61885ee9ce5e7de52900b50536bd1809bc4
ohasanov-hbrw <ka9an1sn00b@gmail.com>
2022-10-17 18:49:50 +0300

    oh boy

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-
+#define _WIN32_WINNT 0x0501
 #include <math.h>
 #include <vector>
 #include <algorithm>
--- windowsmakeall.sh
+++ windowsmakeall.sh
@@ -1,3 +1,3 @@
 make clean
-make deps CC="i686-w64-mingw32-gcc -m32 --static" -j12
-make run CC="i686-w64-mingw32-g++ -m32 --static" name="osus.exe" ldflags="-Lvendor/raylib/src -lraylib -Lvendor/zip/build -lzip -lpthread -lwinmm -lgdi32 -lopengl32" -j12
+make deps CC="i686-w64-mingw32-gcc --static" -j12
+make run CC="i686-w64-mingw32-g++ --static" name="osus.exe" ldflags="-Lvendor/raylib/src -lraylib -Lvendor/zip/build -lzip -lpthread -lwinmm -lgdi32 -lopengl32" -j12
